324960a9ac8beed87a700be39ab829808c350d4d
Richard Bui <rbui1@student.monash.edu>
2020-04-25 16:39:43 +1000

    Added Login GUI form

---
+++ src/LoginForm.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel LoginPanel;
+    private JTextField pracIDField;
+    private JLabel pracIDLabel;
+    private JButton loginButton;
+}
