83d69d112d689a6e1295c6dba62dc9065d85e1e9
Richard Bui <rbui1@student.monash.edu>
2020-05-13 14:29:28 +1000

    Generalised periodic measurement calls class

--- src/main/java/com/teamohno/PeriodicCholesterolCall.java
+++ src/main/java/com/teamohno/PeriodicCholesterolCall.java
@@ -3,27 +3,14 @@ package com.teamohno;
 import java.util.ArrayList;
 import java.util.TimerTask;
 
-public class PeriodicCholesterolCall extends TimerTask {
-    public static int iteration;
-
-    // put a static variable inside parent class (MeasurementCholesterolCall) -> so this would cause all meassurement periodic calls to have saem frequency?
-    public static int frequency;
-
-    private ArrayList<PatientSubject> patientSubjectList;
-
+public class PeriodicCholesterolCall extends PeriodicMeasurementCall {
     public PeriodicCholesterolCall(ArrayList<PatientSubject> newPatientSubjectList){
-        patientSubjectList = newPatientSubjectList;
-        iteration = 0;
-        frequency = 2000;
+        super(newPatientSubjectList, Measurement.Type.CHOLESTEROL);
     }
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(frequency);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        super.run();
 
         for (int i = 0; i < patientSubjectList.size(); i++) {
             //commented out to test without server
@@ -33,7 +20,5 @@ public class PeriodicCholesterolCall extends TimerTask {
             System.out.println("Iteration " + iteration + ", Finding cholesterol/measurement for patient " + i);
             System.out.println("patient " + i + " , name:" + patientSubjectList.get(i).getState().getFirstName());
         }
-
-        iteration++;
     }
 }
---
+++ src/main/java/com/teamohno/PeriodicMeasurementCall.java
@@ -0,0 +1,35 @@
+package com.teamohno;
+
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+public abstract class PeriodicMeasurementCall extends TimerTask {
+    public static int iteration;
+
+    // put a static variable inside parent class (MeasurementCholesterolCall) -> so this would cause all meassurement periodic calls to have saem frequency?
+    public static int frequency;
+    protected Measurement.Type type;
+    protected ArrayList<PatientSubject> patientSubjectList;
+
+    protected PeriodicMeasurementCall(){}
+
+    protected PeriodicMeasurementCall(ArrayList<PatientSubject> newPatientSubjectList, Measurement.Type typeMeasurement){
+        patientSubjectList = newPatientSubjectList;
+        type = typeMeasurement;
+
+        iteration = 0;
+        // default 2 seconds
+        frequency = 2000;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(frequency);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Iteration " + iteration + ", Repeat measurement calls");
+        iteration++;
+    }
+}
